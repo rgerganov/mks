7a74bdadd04b128eb1fa377e169958df96e4e1ac
Radoslav Gerganov <rgerganov@vmware.com>
2015-08-03 16:56:04 +0300

    add special support for vSphere 6

--- mks.py
+++ mks.py
@@ -65,6 +65,27 @@ def parse(url):
         pwd = getpass.getpass()
     return p.username, pwd, p.hostname, port, query
 
+def vsphere_url(vm, host, args):
+    # Generates console URL for vSphere versions prior 6.0
+    ticket = vm.AcquireTicket('mks')
+    vm_host = ticket.host if ticket.host else host
+    path = '?host={0}&port={1}&ticket={2}&cfgFile={3}&thumbprint={4}'.format(
+        vm_host, ticket.port, ticket.ticket, ticket.cfgFile,
+        ticket.sslThumbprint)
+    base_url = "http://rgerganov.github.io/noVNC/5"
+    url = "{0}/vnc_auto.html?host={1}&port={2}&path={3}".format(base_url,
+                                args.mhost, args.mport, urllib.quote(path))
+    return url
+
+def vsphere6_url(vm, host):
+    # Generates console URL for vSphere 6
+    ticket = vm.AcquireTicket('webmks')
+    vm_host = ticket.host if ticket.host else host
+    path = "ticket/" + ticket.ticket
+    base_url = "https://rgerganov.github.io/noVNC/6"
+    url = "{0}/vnc_auto.html?host={1}&path={2}".format(base_url, vm_host, urllib.quote(path))
+    return url
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("url",
@@ -100,14 +121,10 @@ def main():
 
     if not vm:
         err('Cannot find the specified VM')
-    ticket = vm.AcquireTicket('mks')
-    vm_host = ticket.host if ticket.host else host
-    path = '?host={0}&port={1}&ticket={2}&cfgFile={3}&thumbprint={4}'.format(
-        vm_host, ticket.port, ticket.ticket, ticket.cfgFile,
-        ticket.sslThumbprint)
-    base_url = "http://rgerganov.github.io/noVNC/"
-    url = "{0}/vnc_auto.html?host={1}&port={2}&path={3}".format(base_url,
-                                args.mhost, args.mport, urllib.quote(path))
+    if si.content.about.version.startswith("6"):
+        url = vsphere6_url(vm, host)
+    else:
+        url = vsphere_url(vm, host, args)
     webbrowser.open(url)
 
 if __name__ == '__main__':
